49ad01d76b793ec44e82bd3ee5d61088c805bb8a
iamconnaught <wdonnelly22@gmail.com>
2019-04-17 15:42:19 -0500

    added mongoose create model

--- controllers/carController.js
+++ controllers/carController.js
@@ -13,6 +13,19 @@ router.get('/new', (req, res) => {
 router.post('/', (req, res) => {
   console.log(req.body, "<-- req.body will have the contents of the form");
 
+  if(req.body.isConvertible === 'on'){
+    req.body.isConvertible = true;
+  } else {
+    req.body.isConvertible = false
+  }
+
+  Car.create(req.body, (err, createdCarFromTheDB) => {
+    if(err){
+      res.send(err);
+    } else {
+      res.redirect('/cars');
+    }
+  })
 });
 
 module.exports = router
