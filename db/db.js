67a23b56485ebb525ea03525f8eb50461158d5ea
iamconnaught <wdonnelly22@gmail.com>
2019-04-17 15:30:14 -0500

    Connected express to mongo

---
+++ db/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const connectionString = 'mongodb://localhost/cars'
+
+
+mongoose.connect(connectionString, {
+ useNewUrlParser: true,
+ useCreateIndex: true
+  });
+
+mongoose.connection.on('connected', ()=>{
+	console.log(`mongoose connected to ${connectionString}`);
+});
+
+mongoose.connection.on('disconnected', ()=>{
+	console.log(`mongoose disconnected to ${connectionString}`);
+});
+
+mongoose.connection.on('error', (err)=>{
+	console.log(`mongoose err: ${err}`);
+});
+
